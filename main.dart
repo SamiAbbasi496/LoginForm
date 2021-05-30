4108bdacb1083cd47df75a9c660b3a042b757c3e
samiabbasi496 <samiabbasi496@gmail.com>
2021-05-30 19:15:04 +0500

    Login Form

---
+++ main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Login Page"),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 200,
+                  child: TextField(),
+                ),
+                Container(
+                  width: 200,
+                  child: TextField(),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login")),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Forgot Password"))
+              ],
+            ),
+          ),
+        ));
+  }
+}
